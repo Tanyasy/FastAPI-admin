c6cd07d1d72e1b4d7634df4764b5bdd6998bb15d
sz26_hejie <1303148074@qq.com>
2021-01-12 23:38:39 +0800

    初始化FastAPI-admin项目

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from starlette.requests import Request
 
 from core import config
@@ -12,6 +14,29 @@ from app.db.base_class import Base
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title=config.PROJECT_NAME, openapi_url="/api/v1/openapi.json")
 app.include_router(api_router, prefix=config.API_V1_STR)
+
+
+@app.exception_handler(RequestValidationError)
+async def request_validation_exception_handler(
+        request: Request, exc: RequestValidationError
+) -> JSONResponse:
+    """
+    封装参数校验失败的响应信息
+    :param request:
+    :param exc:
+    :return:
+    """
+    message = ""
+    for items in exc.errors():
+        message += items.get("type") + ": " + items.get("msg") + "\n"
+    return JSONResponse(
+        content={
+            "status": status.HTTP_400_BAD_REQUEST,
+            "message": message
+        },
+        status_code=status.HTTP_400_BAD_REQUEST
+    )
+    pass
 
 
 @app.middleware("http")
