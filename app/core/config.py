50523011a49fe860cc718637be4acec659603bd2
sz26_hejie <1303148074@qq.com>
2021-02-14 23:06:54 +0800

    更改项目路径获取方式

--- app/core/config.py
+++ app/core/config.py
@@ -27,6 +27,6 @@ DATABASE_URI = (
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 60 minutes * 24 hours * 8 days = 8 days
 
 # upload file
-BASE_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__name__))))
 UPLOAD_PATH = os.path.join(BASE_DIR, "download")
 ACCEPT_FILE_TYPE = ["xls", "csv"]
